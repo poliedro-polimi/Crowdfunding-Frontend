60be76e03cacfc280563d8aa4d8b6a1e5caf6571
Stefano Campanella <stefano.campanella@globalcom.tv.it>
2016-10-10 18:51:36 +0200

    Sistemata view per gli errori fatali

--- includes/full_error.php
+++ includes/full_error.php
@@ -1,8 +1,16 @@
 <?php
-/* @var $exception BSSException */
+/* @var $exception Exception */
 
-include(__DIR__.'/inc/header.html.inc.php');
+use site\db\DBException;
+use site\Exception;
+use site\PHPErrorException;
+use site\Site;
+use site\views\Html;
+
 ?>
+<!doctype html>
+<html>
+<head>
 <style type="text/css">
 
 </style>
@@ -13,19 +21,18 @@ include(__DIR__.'/inc/header.html.inc.php');
 <h2>Si &egrave; verificato un errore grave che impedisce la corretta esecuzione del Sito Web.</h2>
 <h3>Ci scusiamo per il disagio, il nostro staff &egrave; stato notificato e il problema sar&agrave; presto risolto.</h3>
 <p>Se avete necessit&agrave; di contattare lo staff potete farlo inviando un email a: <a
-    href="mailto:bsoulshippuden@gdr.rel.to">bsoulshippuden@gdr.rel.to</a></p>
-<p>Dettaglio dell&#039;errore: <?php echo (($exception!=null and
-    $exception instanceof Exception and $exception->getMessage())?
-    escape($exception->getMessage(), 'html'):
-    'Nessuno'); ?></p>
+    href="mailto:<?= Site::getParam('email') ?>"><?= Html::escape(Site::getParam('email')); ?></a></p>
+<p>Dettaglio dell&#039;errore: <?php
+  echo (($exception!=null and $exception instanceof Exception and $exception->getMessage())?
+    Html::escape($exception->getMessage()):'Nessuno'); ?></p>
 <?php
 if(defined('DEBUG') and DEBUG and $exception!=null){
   if($exception instanceof Exception) {
     echo '<table><tr><th colspan="2">Exception</th></tr>
     <tr><td>Nome</td><td>' . get_class($exception) . '</td></tr>
-    <tr><td>Messaggio</td><td>' . escape($exception->getMessage(), 'html') . '</td></tr>';
-    if($exception instanceof BSSException) {
-      echo '<tr><td>Messaggio Interno</td><td>' . nl2br(escape($exception->getInternalError(), 'html')) . '</td></tr>';
+    <tr><td>Messaggio</td><td>' . Html::escape($exception->getMessage(), 'html') . '</td></tr>';
+    if($exception instanceof Exception) {
+      echo '<tr><td>Messaggio Interno</td><td>' . nl2br(Html::escape($exception->getInternalError(), 'html')) . '</td></tr>';
     }
     if ($exception instanceof PHPErrorException) {
       echo '<tr><td>Contesto</td><td>' . $exception->getContextAsString() . '</td></tr>';
