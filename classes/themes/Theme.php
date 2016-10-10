90de8890fbcaa95de47cab2a506467175f1abe95
Stefano Campanella <stefano.campanella@globalcom.tv.it>
2016-10-10 21:32:18 +0200

    Finita la base del tema

--- classes/themes/Theme.php
+++ classes/themes/Theme.php
@@ -1,6 +1,9 @@
 <?php
 namespace site\theme;
 
+use site\Site;
+use site\views\Html;
+
 class Theme implements ThemeInterface {
     private $title;
     private $head;
@@ -10,12 +13,27 @@ class Theme implements ThemeInterface {
 
     public function append($str, $part = 'body')
     {
-        // TODO: Implement append() method.
+        if(isset($this->$part)){
+            $this->$part .= $str;
+        }
     }
 
     public function render()
     {
-        echo <<<HTML
+        $this->title .= ' - '.Site::getParam('site_name');
+
+        $ready = '';
+        if(!empty($this->script_on_ready)){
+            $ready = <<<READY
+<script type="application/javascript">
+$(function(){
+    {$this->script_on_ready}
+});
+</script>
+READY;
+        }
+
+        echo '
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -23,27 +41,32 @@ class Theme implements ThemeInterface {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Bootstrap 101 Template</title>
+    <title>'.Html::escape($this->title).'</title>
 
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!-- WARNING: Respond.js doesnt work if you view the page via file:// -->
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+    '.$this->head.'
   </head>
   <body>
-    <h1>Hello, world!</h1>
+    '.$this->body.'
 
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <!-- jQuery (necessary for Bootstraps JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="js/bootstrap.min.js"></script>
+    
+    '.$this->script.'
+    
+    '.$ready.'
   </body>
 </html>
-HTML;
+';
     }
 }
