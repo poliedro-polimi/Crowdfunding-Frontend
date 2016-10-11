20393ac13a6316e792fde47991d4b09e3a61c9ef
Stefano Campanella <stefano.campanella@globalcom.tv.it>
2016-10-12 00:39:58 +0200

    Aggiunto tema ajax json

---
+++ classes/themes/AjaxTheme.php
@@ -0,0 +1,26 @@
+<?php
+namespace nigiri\themes;
+
+/**
+ * Implements rendering of data only in JSON format. Useful for Ajax Backends
+ * It is intended to be used with an explicit call to Site::switchTheme() in the controller that implements the ajax backend
+ *
+ * @package nigiri\themes
+ */
+class AjaxTheme implements ThemeInterface {
+    private $json=null;
+
+    public function append($str, $part = 'body')
+    {
+        return;
+    }
+
+    public function jsonOutput($data){
+        $this->json = $data;
+    }
+
+    public function render()
+    {
+        echo json_encode($this->json);
+    }
+}
