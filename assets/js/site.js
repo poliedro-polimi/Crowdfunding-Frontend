2eb159a681d43ddd32adda1443d0955965a626fb
Stefano Campanella <stefano.campanella@globalcom.tv.it>
2018-03-20 23:39:39 +0100

    Aggiunto js per i link esterni e risistemazione di altro codice js

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,26 +1,19 @@
 var countDownGoal = new Date('2018-06-30 15:00:00');
 
 $(function () {
-    //Navbar Code
-    var obj = $("section#header");
-    var navbar = $("nav#mainNav");
-    var togglebtn = $("button.navbar-toggle");
 
-    $(window).scroll(function () {
-        var top = $(window).scrollTop();
-        var max = obj.height() * .90;
-
-        if (togglebtn.is(":visible") && !togglebtn.hasClass("collapsed"))
-            return;
-
-        if (top >= max && !navbar.hasClass("affix"))
-            navbar.addClass("affix");
-        else if (top < max && navbar.hasClass("affix"))
-            navbar.removeClass("affix");
+    $(document).on('click', 'a.extern', function(ev){
+        ev.preventDefault();
+        window.open($(this).attr('href'));
     });
+
+    $(window).scroll(navBarState);
 
     togglebtn.click(function () {
         setTimeout(function () {
+            var navbar = $("nav#mainNav");
+            var togglebtn = $("button.navbar-toggle");
+
             if (!togglebtn.hasClass("collapsed")) {
                 navbar.addClass("menu-open");
                 if (!navbar.hasClass("affix"))
@@ -30,11 +23,28 @@ $(function () {
             }
         }, 100);
     });
+    navBarState();
 
     //CountDown Code
     updateCountDown();
     setInterval(updateCountDown, 1000);
 });
+
+function navBarState(){
+    var obj = $("section#header");
+    var navbar = $("nav#mainNav");
+    var togglebtn = $("button.navbar-toggle");
+    var top = $(window).scrollTop();
+    var max = obj.height() * .90;
+
+    if (togglebtn.is(":visible") && !togglebtn.hasClass("collapsed"))
+        return;
+
+    if (top >= max && !navbar.hasClass("affix"))
+        navbar.addClass("affix");
+    else if (top < max && navbar.hasClass("affix"))
+        navbar.removeClass("affix");
+}
 
 function updateCountDown(){
     var $countd = $('#countdown');
