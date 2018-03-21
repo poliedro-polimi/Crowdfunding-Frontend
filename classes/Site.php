d2f24efde634194aa9945011ea1af172088de0a6
Stefano Campanella <stefano.campanella@globalcom.tv.it>
2018-03-21 01:31:17 +0100

    Added configuration for gettext in the Site init function

--- classes/Site.php
+++ classes/Site.php
@@ -66,10 +66,20 @@ class Site{
             ini_set('display_errors', true);
         }
 
-        //TODO how to determine language since now? Is Router already configured?
         $locale = self::getParam('locales', []);
         if(array_key_exists(self::getRouter()->getRequestedLanguage(), $locale)) {
             call_user_func_array('setlocale', array_merge([LC_ALL], $locale[self::getRouter()->getRequestedLanguage()]));
+
+            if(function_exists('gettext') and file_exists(dirname(__DIR__).'/i18n')){//Internationalization folder for gettext
+                $directory = dirname(__DIR__).'/i18n';
+                $domain = 'messages';
+                $i18nLocale = reset($locale[self::getRouter()->getRequestedLanguage()]);
+
+                setlocale( LC_MESSAGES, $i18nLocale);
+                bindtextdomain($domain, $directory);
+                textdomain($domain);
+                bind_textdomain_codeset($domain, 'UTF-8');
+            }
         }
 
         date_default_timezone_set(self::getParam('timezone', ''));
