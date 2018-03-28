ae052ccbf75235effc1c31af9437ae7dff6a9989
Stefano Campanella <stefano.campanella@globalcom.tv.it>
2018-03-28 12:33:28 +0200

    Finished donation page layout. It's still non functioning

--- assets/js/payment.js
+++ assets/js/payment.js
@@ -1,65 +1,67 @@
 var BACKEND_URL = 'https://poliedropolimi.pythonanywhere.com/paypal';
 
 paypal.Button.render({
-  env: 'sandbox',
-  locale: 'it_IT',
-  commit: true,
-  style: {
-    color: 'gold',
-    size: 'large'
-  },
-  payment: function(data, actions){
-    return $.ajax({
-      url: BACKEND_URL+'/create',
-      method: 'post',
-      crossDomain: true,
-      contentType: 'application/json; charset=UTF-8',
-      dataType: 'json',
-      jsonp: false,
-      data: JSON.stringify({
-        donation: 2,
-        stretch_goal: 0,
-        items: 0,
-        shirts: [],
-        notes: '',
-        lang: 'it',
-        reference: {
-          firstname: 'Tester',
-          lastname: 'Testing',
-          email: 'info-buy@poliedro-polimi.it',
-          phone: '3342752215'
-        }
-      })
-    }).then(function(data) {
-      if(typeof(data.paymentID)!='undefined') {
-        return data.paymentID;
-      }
-      else{
-        return null;//Just guessing...PayPal Documentation on error handling is null
-      }
-    });
-  },
-  onAuthorize: function(data, actions){
-    return $.ajax({
-      url: BACKEND_URL+'/execute',
-      method: 'post',
-      crossDomain: true,
-      contentType: 'application/json; charset=UTF-8',
-      dataType: 'json',
-      jsonp: false,
-      data: JSON.stringify({
-        lang: 'it',
-        paymentID: data.paymentID,
-        payerID: data.payerID
-      })
-    }).then(function(){
-      //TODO redirect?
-    });
-  },
-  onCancel: function(data, actions){
+    env: 'sandbox',
+    locale: 'it_IT',
+    commit: true,
+    style: {
+        color: 'gold',
+        size: 'large',
+        shape: 'rect',
+        label: 'paypal'
+    },
+    payment: function(data, actions){
+        return $.ajax({
+            url: BACKEND_URL+'/create',
+            method: 'post',
+            crossDomain: true,
+            contentType: 'application/json; charset=UTF-8',
+            dataType: 'json',
+            jsonp: false,
+            data: JSON.stringify({
+                donation: 2,
+                stretch_goal: 0,
+                items: 0,
+                shirts: [],
+                notes: '',
+                lang: 'it',
+                reference: {
+                    firstname: 'Tester',
+                    lastname: 'Testing',
+                    email: 'info-buy@poliedro-polimi.it',
+                    phone: '3342752215'
+                }
+            })
+        }).then(function(data) {
+            if(typeof(data.paymentID)!='undefined') {
+                return data.paymentID;
+            }
+            else{
+                return null;//Just guessing...PayPal Documentation on error handling is null
+            }
+        });
+    },
+    onAuthorize: function(data, actions){
+        return $.ajax({
+            url: BACKEND_URL+'/execute',
+            method: 'post',
+            crossDomain: true,
+            contentType: 'application/json; charset=UTF-8',
+            dataType: 'json',
+            jsonp: false,
+            data: JSON.stringify({
+                lang: 'it',
+                paymentID: data.paymentID,
+                payerID: data.payerID
+            })
+        }).then(function(){
+            //TODO redirect?
+        });
+    },
+    onCancel: function(data, actions){
 
-  },
-  onError: function(data, actions){
+    },
+    onError: function(data, actions){
 
-  }
+    }
 }, '#pay-button');
--- views/donation/index.php
+++ views/donation/index.php
@@ -31,7 +31,7 @@ define('REWARD_THRESHOLD3', 10);
 </section>
 
 <section id="donation_data">
-    <form>
+    <div id="general_data">
         <div class="form-inline">
             <div class="form-group">
                 <label for="nome"><?= l('Nome'); ?>:</label>
@@ -66,106 +66,106 @@ define('REWARD_THRESHOLD3', 10);
                 </div>
             </div>
         </div>
-        <fieldset>
-            <legend><?= l('Ricompensa') ?></legend>
-            <p>Per ringraziarti della tua donazione a PoliEdro puoi scegliere tra i seguenti pacchetti di gadget:</p>
-            <div class="radio">
-                <label>
-                    <input type="radio" name="chosenReward" id="reward0" value="0" checked="checked" />
-                    Non desidero alcuna ricompensa
-                </label>
-            </div>
-            <div class="form-inline">
-                <label class="radio-inline">
-                    <input type="radio" name="chosenReward" id="reward1" value="1" disabled="disabled"
-                           data-threshold="<?= REWARD_THRESHOLD1 ?>" />
-                    Adesivi e Spilla PoliMi Pride (donazione minima <?= REWARD_THRESHOLD1 ?>&euro;)
-                </label>
-                <div class="form-group qty">
-                    <label class="sr-only" for="qty1">Quantità</label>
-                    <div class="input-group">
-                        <div class="input-group-addon"><?= Html::escape(l('Qtà')) ?></div>
-                        <input type="number" name="qty1" id="qty1" class="form-control" value="0" />
-                    </div>
+    </div>
+    <fieldset>
+        <legend><?= l('Ricompensa') ?></legend>
+        <p>Per ringraziarti della tua donazione a PoliEdro puoi scegliere tra i seguenti pacchetti di gadget:</p>
+        <div class="radio">
+            <label>
+                <input type="radio" name="chosenReward" id="reward0" value="0" checked="checked" />
+                Non desidero alcuna ricompensa
+            </label>
+        </div>
+        <div class="form-inline">
+            <label class="radio-inline">
+                <input type="radio" name="chosenReward" id="reward1" value="1" disabled="disabled"
+                       data-threshold="<?= REWARD_THRESHOLD1 ?>" />
+                Adesivi e Spilla PoliMi Pride (donazione minima <?= REWARD_THRESHOLD1 ?>&euro;)
+            </label>
+            <div class="form-group qty">
+                <label class="sr-only" for="qty1">Quantità</label>
+                <div class="input-group">
+                    <div class="input-group-addon"><?= Html::escape(l('Qtà')) ?></div>
+                    <input type="number" name="qty1" id="qty1" class="form-control" value="0" />
                 </div>
             </div>
-            <div class="form-inline">
-                <label class="radio-inline">
-                    <input type="radio" name="chosenReward" id="reward2" value="2" disabled="disabled"
-                           data-threshold="<?= REWARD_THRESHOLD2 ?>" />
-                    Adesivi, Spilla e Shopper PoliMi Pride (donazione minima <?= REWARD_THRESHOLD2 ?>&euro;)
-                </label>
-                <div class="form-group qty">
-                    <label class="sr-only" for="qty2">Quantità</label>
-                    <div class="input-group">
-                        <div class="input-group-addon"><?= Html::escape(l('Qtà')) ?></div>
-                        <input type="number" name="qty2" id="qty2" class="form-control" value="0" />
-                    </div>
+        </div>
+        <div class="form-inline">
+            <label class="radio-inline">
+                <input type="radio" name="chosenReward" id="reward2" value="2" disabled="disabled"
+                       data-threshold="<?= REWARD_THRESHOLD2 ?>" />
+                Adesivi, Spilla e Shopper PoliMi Pride (donazione minima <?= REWARD_THRESHOLD2 ?>&euro;)
+            </label>
+            <div class="form-group qty">
+                <label class="sr-only" for="qty2">Quantità</label>
+                <div class="input-group">
+                    <div class="input-group-addon"><?= Html::escape(l('Qtà')) ?></div>
+                    <input type="number" name="qty2" id="qty2" class="form-control" value="0" />
                 </div>
             </div>
-            <div class="form-inline">
-                <label class="radio-inline">
-                    <input type="radio" name="chosenReward" id="reward3" value="3" disabled="disabled" data-threshold="<?= REWARD_THRESHOLD3 ?>" />
-                    Kit PoliMi Pride Completo: Adesivi, Spilla, Shopper e T-Shirt (donazione minima <?=
-                    REWARD_THRESHOLD3 ?>&euro;)
-                </label>
-                <div class="form-group qty">
-                    <label class="sr-only" for="qty3">Quantità</label>
-                    <div class="input-group">
-                        <div class="input-group-addon"><?= Html::escape(l('Qtà')) ?></div>
-                        <input type="number" name="qty3" id="qty3" class="form-control" value="0" />
-                    </div>
+        </div>
+        <div class="form-inline">
+            <label class="radio-inline">
+                <input type="radio" name="chosenReward" id="reward3" value="3" disabled="disabled" data-threshold="<?= REWARD_THRESHOLD3 ?>" />
+                Kit PoliMi Pride Completo: Adesivi, Spilla, Shopper e T-Shirt (donazione minima <?=
+                REWARD_THRESHOLD3 ?>&euro;)
+            </label>
+            <div class="form-group qty">
+                <label class="sr-only" for="qty3">Quantità</label>
+                <div class="input-group">
+                    <div class="input-group-addon"><?= Html::escape(l('Qtà')) ?></div>
+                    <input type="number" name="qty3" id="qty3" class="form-control" value="0" />
                 </div>
             </div>
-            <fieldset id="additional_data">
-                <legend>Scegli la tua T-Shirt</legend>
-                <div class="tshirt-chooser col-xs-3">
-                    <div class="radio">
-                        <label>
-                            <input type="radio" name="shirt-type" value="t-shirt" />
-                            T-shirt classica<br /><img src="https://ae01.alicdn.com/kf/HTB1FnwySFXXXXa0XVXXq6xXFXXXo/Men-s-Leisure-Rainbow-Sheep-Of-The-Family-LGBT-T-shirt-White-Short-Sleeve-Custom-T.jpg_640x640.jpg" />
-                        </label>
-                    </div>
-                    <div class="radio">
-                        <label>
-                            <input type="radio" name="shirt-type" value="tank-top" />
-                            Canotta<br /><img src="https://www.pianetaoutlet.it/59072-large_default/adidas-trefoil-tank-canotta-uomo-nera.jpg" />
-                        </label>
-                    </div>
-                    <div class="form-group">
-                        <label for="shirt-size">Scegli la taglia:</label>
-                        <select class="form-control" name="shirt-size" id="shirt-size">
-                            <option value="XS">XS</option>
-                            <option value="S">S</option>
-                            <option value="M">M</option>
-                            <option value="L">L</option>
-                            <option value="XL">XL</option>
-                            <option value="XXL">XXL</option>
-                        </select>
-                    </div>
-                </div>
-            </fieldset>
-            <fieldset id="location_data">
-                <legend>Indica dove preferisci ritirare le tue ricompense</legend>
-                <p>Per ragioni organizzative non possiamo spedire gli oggetti a un tuo domicilio, li
-                    consegneremo invece personalmente presso i nostri stand nelle sedi del Politecnico organizzati
-                    durante il mese di Giugno</p>
+        </div>
+        <fieldset id="tshirt_data">
+            <legend>Scegli la tua T-Shirt</legend>
+            <div class="tshirt_chooser col-xs-3">
                 <div class="radio">
                     <label>
-                        <input type="radio" name="location" value="leonardo" />Leonardo
+                        <input type="radio" name="shirt-type" value="t-shirt" />
+                        T-shirt classica<br /><img src="https://ae01.alicdn.com/kf/HTB1FnwySFXXXXa0XVXXq6xXFXXXo/Men-s-Leisure-Rainbow-Sheep-Of-The-Family-LGBT-T-shirt-White-Short-Sleeve-Custom-T.jpg_640x640.jpg" />
                     </label>
                 </div>
                 <div class="radio">
-                    <label><input type="radio" name="location" value="bovisa" />Bovisa</label>
+                    <label>
+                        <input type="radio" name="shirt-type" value="tank-top" />
+                        Canotta<br /><img src="https://www.pianetaoutlet.it/59072-large_default/adidas-trefoil-tank-canotta-uomo-nera.jpg" />
+                    </label>
                 </div>
-            </fieldset>
+                <div class="form-group">
+                    <label for="shirt-size">Scegli la taglia:</label>
+                    <select class="form-control" name="shirt-size" id="shirt-size">
+                        <option value="XS">XS</option>
+                        <option value="S">S</option>
+                        <option value="M">M</option>
+                        <option value="L">L</option>
+                        <option value="XL">XL</option>
+                        <option value="XXL">XXL</option>
+                    </select>
+                </div>
+            </div>
         </fieldset>
-        <div class="form-group">
-            <label for="notes">Note Addizionali</label>
-            <textarea class="form-control" rows="3" id="notes"></textarea>
-        </div>
-        <div id="pay-button"></div>
-    </form>
+        <fieldset id="location_data">
+            <legend>Indica dove preferisci ritirare le tue ricompense</legend>
+            <p>Per ragioni organizzative non possiamo spedire gli oggetti a un tuo domicilio, li
+                consegneremo invece personalmente presso i nostri stand nelle sedi del Politecnico organizzati
+                durante il mese di Giugno</p>
+            <div class="radio">
+                <label>
+                    <input type="radio" name="location" value="leonardo" />Leonardo
+                </label>
+            </div>
+            <div class="radio">
+                <label><input type="radio" name="location" value="bovisa" />Bovisa</label>
+            </div>
+        </fieldset>
+    </fieldset>
+    <div class="form-group">
+        <label for="notes">Note Addizionali</label>
+        <textarea class="form-control" rows="3" id="notes"></textarea>
+    </div>
+    <div id="pay-button"></div>
 </section>
 </div>
 
@@ -189,10 +189,10 @@ amountChange('.$amount.');
 
 $("input[name=chosenReward]").change(function(){
     if($("#reward3").prop("checked")){
-        $("#additional_data").show().find("input select").prop("disabled", false);
+        $("#tshirt_data").show().find("input select").prop("disabled", false);
     }
     else{
-        $("#additional_data").hide().find("input select").prop("disabled", true);        
+        $("#tshirt_data").hide().find("input select").prop("disabled", true);        
     }
     
     if(!$("#reward0").prop("checked")) {
@@ -208,6 +208,7 @@ $("input[name=chosenReward]").change(function(){
     var $qty = $t.closest(".form-inline").find(".qty input");
     if($qty.length>0) {
         $qty.val(Math.floor($("#amount").val() / $t.data("threshold")));
+        $qty.change();
     }
 });
 
@@ -222,7 +223,29 @@ $(".qty input").change(function(ev){
         $t.closest(".qty").addClass("has-warning");
         setTimeout(function(){$t.closest(".qty").removeClass("has-warning");}, 2000);
     }
+    
+    if($t.attr("id")=="qty3"){
+        tshirtSection($t.val());
+    }
 });
+
+function tshirtSection(qty){
+    var currentSections = $(".tshirt_chooser");
+    var $copy;
+    var diff = qty - currentSections.length;
+    
+    if(diff > 0){
+        for(var i = 0; i < diff; i++){
+            $copy = currentSections.first().clone();
+            $copy.find("input[type=radio]").attr("name", "shirt-type"+(currentSections.length+i)).prop("checked", false);
+            $copy.find("option").prop("selected", false);
+            $copy.appendTo("#tshirt_data");
+        }
+    }
+    else if(diff < 0){
+        currentSections.slice(diff).remove();
+    }
+}
 
 function amountChange(value) {
     $("#handle-label span").text(value+"€");
