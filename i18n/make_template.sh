21e55256b49835281be0e1ea24f2ecf5332a24db
Stefano Campanella <stefano.campanella@globalcom.tv.it>
2018-04-06 17:39:47 +0200

    Fixing wrong POT generation script

--- i18n/make_template.sh
+++ i18n/make_template.sh
@@ -1,16 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PROJECT_DIR="$( dirname "$DIR" )"
 
-find $DIR/.. -type f -name *.php > phps
-
-if [ -f messages.pot ]
+if [ -f $DIR/messages.pot ]
 then
-    mv messages.pot messages.po
+    mv $DIR/messages.pot $DIR/messages.po
 fi
 
-xgettext -L PHP --keyword=l:1 -F --copyright-holder=PoliEdro -j -f phps -d messages -p $DIR
+find $PROJECT_DIR -iname "*.php" | xargs xgettext -L PHP --keyword=l:1 -F --copyright-holder=PoliEdro -j -d messages -p $DIR --from-code=UTF-8
 
-mv messages.po messages.pot
-
-rm phps
+mv $DIR/messages.po $DIR/messages.pot
