23fba87fc073e6ace628352019e7e7ae03bac8cb
Stefano Campanella <stefano.campanella@globalcom.tv.it>
2018-04-06 17:11:16 +0200

    Updated script to generate the pot translation template

--- i18n/make_template.sh
+++ i18n/make_template.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
-find .. -type f -name *.php > phps
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-xgettext -L PHP --keyword=l:1 -F --copyright-holder=PoliEdro -j -f phps -d messages -p .
+find $DIR/.. -type f -name *.php > phps
+
+if [ -f messages.pot ]
+then
+    mv messages.pot messages.po
+fi
+
+xgettext -L PHP --keyword=l:1 -F --copyright-holder=PoliEdro -j -f phps -d messages -p $DIR
 
 mv messages.po messages.pot
 
