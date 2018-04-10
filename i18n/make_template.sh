cf4c3d8f2850a294b4ff23a1ad91a25b83bfc964
Stefano Campanella <stefano.campanella@globalcom.tv.it>
2018-04-10 03:21:14 +0200

    Update .pot generation script to avoid error if pot doesn't exist

--- i18n/make_template.sh
+++ i18n/make_template.sh
@@ -3,11 +3,14 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PROJECT_DIR="$( dirname "$DIR" )"
 
+JOIN=""
+
 if [ -f $DIR/messages.pot ]
 then
     mv $DIR/messages.pot $DIR/messages.po
+    JOIN="-j"
 fi
 
-find $PROJECT_DIR -iname "*.php" | xargs xgettext -L PHP --keyword=l:1 -F --copyright-holder=PoliEdro -j -d messages -p $DIR --from-code=UTF-8
+find $PROJECT_DIR -iname "*.php" | xargs xgettext -L PHP --keyword=l:1 -F --copyright-holder=PoliEdro $JOIN -d messages -p $DIR --from-code=UTF-8
 
 mv $DIR/messages.po $DIR/messages.pot
