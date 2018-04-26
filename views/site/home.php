772aba8e2593e8a760b31a56190deaab905f34a1
Davide Depau <davide@depau.eu>
2018-04-26 16:06:48 +0200

    Adjust included reward bubbles position

--- views/site/home.php
+++ views/site/home.php
@@ -4,11 +4,13 @@ use nigiri\Site;
 use nigiri\views\Url;
 use nigiri\views\Html;
 use site\controllers\DonationController;
+
 ?>
 <section id="how_to_help">
     <div class="container container-narrow">
         <h1 class="section-title"><?= Html::escape(l('Come sostenere il progetto?')); ?></h1>
-        <div class="section-content"><?= Html::escape(l('Quest\'anno PoliMi Pride ha tanti gadget colorati in più! Le sacche/zainetto, gli adesivi e le spille completano il set di gadget targati "Polimi Pride" insieme alla maglietta!')) ?><br /><?= l('Ma per realizzarli abbiamo bisogno del vostro sostegno: lo scorso anno il Politecnico aveva finanziato la realizzazione dei materiali, ma quest\'anno non è più stato possibile farlo, perciò abbiamo lanciato una <strong>campagna di crowdfunding</strong> per raccogliere i fondi per ordinare tutti i materiali PoliMi Pride.') ?>
+        <div class="section-content"><?= Html::escape(l('Quest\'anno PoliMi Pride ha tanti gadget colorati in più! Le sacche/zainetto, gli adesivi e le spille completano il set di gadget targati "Polimi Pride" insieme alla maglietta!')) ?>
+            <br/><?= l('Ma per realizzarli abbiamo bisogno del vostro sostegno: lo scorso anno il Politecnico aveva finanziato la realizzazione dei materiali, ma quest\'anno non è più stato possibile farlo, perciò abbiamo lanciato una <strong>campagna di crowdfunding</strong> per raccogliere i fondi per ordinare tutti i materiali PoliMi Pride.') ?>
         </div>
     </div>
 </section>
@@ -21,14 +23,15 @@ use site\controllers\DonationController;
                 <li><?= Html::escape(l('Fai una donazione a PoliEdro dell\'ammontare che preferisci')) ?></li>
                 <li><?= Html::escape(l('In base all\'ammontare della tua donazione scegli il pacchetto di gadget che più ti piace'))
                     ?></li>
-                <li><?= Html::escape(l('Fino al 10 Giugno il crowdfunding resterà aperto alle donazioni'))?></li>
+                <li><?= Html::escape(l('Fino al 10 Giugno il crowdfunding resterà aperto alle donazioni')) ?></li>
                 <li><?= Html::escape(l('Alla chiusura del crowdfunding partiranno tutti gli ordini dei gadget, che avremo pronti da distribuire durante la Pride Week ai nostri stand nei campus del Politecnico (Leonardo e Bovisa)')) ?></li>
                 <li><?= Html::escape(l('Per ritirare i tuoi gadget presentati agli stand con il tuo numero di donazione o l\'email di conferma')) ?></li>
-            </ol></div>
+            </ol>
+        </div>
     </div>
 </section>
 <section id="sequence">
-    <?= page_include(dirname(dirname(__DIR__)).'/assets/imgs/infographics_'.Site::getRouter()->getRequestedLanguage().'.svg') ?>
+    <?= page_include(dirname(dirname(__DIR__)) . '/assets/imgs/infographics_' . Site::getRouter()->getRequestedLanguage() . '.svg') ?>
 </section>
 <section id="rewards">
     <div class="container container-narrow">
@@ -37,12 +40,13 @@ use site\controllers\DonationController;
     <div class="row justify-content-around no-gutters">
         <div class="col-10 col-md-5 col-lg-3 reward">
             <div class="reward-box">
-                <img src="<?= Url::resource("assets/imgs/sacca.png"); ?>" alt="<?= Html::escape(l('Sacca Zainetto PoliMi Pride')); ?>" />
+                <img src="<?= Url::resource("assets/imgs/sacca.png"); ?>"
+                     alt="<?= Html::escape(l('Sacca Zainetto PoliMi Pride')); ?>"/>
                 <div class="reward-contribution">
                     <div class="contribution-label"><?= Html::escape(l('Contributo Minimo')); ?>:</div>
                     <div class="reward-amount"><?= DonationController::REWARD_THRESHOLD1 ?>&euro;</div>
                 </div>
-                <a href="<?= Url::to('donation', ['reward'=> 1]) ?>"><?= Html::escape(l('Dona')) ?></a>
+                <a href="<?= Url::to('donation', ['reward' => 1]) ?>"><?= Html::escape(l('Dona')) ?></a>
             </div>
             <div class="reward-description">
                 <?= Html::escape(l('Le sacche zainetto PoliMi Pride sono perfette per affrontare la parata del Milano Pride! Le sacche in nylon 34x43cm sono utilissime per portare con sè bottiglie d\'acqua, bandiere e tutto il necessario per essere favolosi!')) ?>
@@ -50,26 +54,39 @@ use site\controllers\DonationController;
         </div>
         <div class="col-10 col-md-5 col-lg-3 reward">
             <div class="reward-box">
-                <img src="<?= Url::resource('assets/imgs/adesivi_spille.png') ?>" alt="Spille, Adesivi e Shopper PoliMi Pride" />
+                <img src="<?= Url::resource('assets/imgs/adesivi_spille.png') ?>"
+                     alt="Spille, Adesivi e Shopper PoliMi Pride"/>
                 <div class="included-rewards">
-                    <img src="<?= Url::resource("assets/imgs/sacca.png"); ?>" alt="<?= Html::escape(l('Sacca Zainetto PoliMi Pride')); ?>" />
+                    <div>
+                        <div class="plus">+</div>
+                        <img src="<?= Url::resource("assets/imgs/sacca.png"); ?>"
+                             alt="<?= Html::escape(l('Sacca Zainetto PoliMi Pride')); ?>"/>
+                    </div>
                 </div>
                 <div class="reward-contribution">
                     <div class="contribution-label"><?= Html::escape(l('Contributo Minimo')); ?>:</div>
                     <div class="reward-amount"><?= DonationController::REWARD_THRESHOLD2 ?>&euro;</div>
                 </div>
-                <a href="<?= Url::to('donation', ['reward'=> 2]) ?>"><?= Html::escape(l('Dona')); ?></a>
+                <a href="<?= Url::to('donation', ['reward' => 2]) ?>"><?= Html::escape(l('Dona')); ?></a>
             </div>
             <div class="reward-description">
-                <?= Html::escape(l("Mostra il tuo orgoglio! Con gli adesivi e la spilla PoliMi Pride potrai esprimere ovunque il tuo supporto all'iniziativa. Usali per personalizzare i tuoi oggetti, dal pc al tuo zaino!")); ?><br /><strong><em><?= Html::escape(l('(Sacca zainetto inclusa)')) ?></em></strong>
+                <?= Html::escape(l("Mostra il tuo orgoglio! Con gli adesivi e la spilla PoliMi Pride potrai esprimere ovunque il tuo supporto all'iniziativa. Usali per personalizzare i tuoi oggetti, dal pc al tuo zaino!")); ?>
+                <br/><strong><em><?= Html::escape(l('(Sacca zainetto inclusa)')) ?></em></strong>
             </div>
         </div>
         <div class="col-10 col-md-6 col-lg-3 reward">
             <div class="reward-box">
-                <img src="<?= Url::resource('assets/imgs/maglia.png') ?>" />
+                <img src="<?= Url::resource('assets/imgs/maglia.png') ?>"/>
                 <div class="included-rewards">
-                    <img src="<?= Url::resource("assets/imgs/sacca.png"); ?>" alt="<?= Html::escape(l('Sacca Zainetto PoliMi Pride')); ?>" />
-                    <img src="<?= Url::resource('assets/imgs/adesivi_spille.png') ?>" alt="Spille, Adesivi e Shopper PoliMi Pride" />
+                    <div>
+                        <img src="<?= Url::resource("assets/imgs/sacca.png"); ?>"
+                             alt="<?= Html::escape(l('Sacca Zainetto PoliMi Pride')); ?>"/>
+                    </div>
+                    <div>
+                        <div class="plus">+</div>
+                        <img src="<?= Url::resource('assets/imgs/adesivi_spille.png') ?>"
+                             alt="Spille, Adesivi e Shopper PoliMi Pride"/>
+                    </div>
                 </div>
                 <div class="reward-contribution">
                     <div class="contribution-label"><?= Html::escape(l('Contributo Minimo')); ?>:</div>
@@ -78,7 +95,8 @@ use site\controllers\DonationController;
                 <a href="<?= Url::to('donation', ['reward' => 3]) ?>"><?= Html::escape(l('Dona')); ?></a>
             </div>
             <div class="reward-description">
-                <?= Html::escape(l("Non perderti la maglietta PoliMi Pride! Con la sua immagine rinnovata è disponibile sia in formato t-shirt che come canotta, scegli quella che fa più per te!")); ?><br /><strong><em><?= Html::escape(l('(Sacca, spilla e adesivi inclusi)')) ?></em></strong>
+                <?= Html::escape(l("Non perderti la maglietta PoliMi Pride! Con la sua immagine rinnovata è disponibile sia in formato t-shirt che come canotta, scegli quella che fa più per te!")); ?>
+                <br/><strong><em><?= Html::escape(l('(Sacca, spilla e adesivi inclusi)')) ?></em></strong>
             </div>
         </div>
     </div>
